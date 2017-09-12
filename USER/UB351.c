435d8e69e9dc2def4e100cfd29abee033b354760
jason xiao <741870940@qq.com>
2017-09-12 13:23:03 +0800

    Jason xiao
    12字节保留字变为4字节

--- USER/UB351.c
+++ USER/UB351.c
@@ -13,14 +13,13 @@ u8 Frame_LenL=0;
 extern uint32_t rx_len;
 u8 Frame_number=0;
 u8 Frame_Head[12]={0xaa,0x55,0xa5,0x5a,Station_Type,Station_Number,0,0,0,0,0,0};//帧头=开始帧+ 节点类型+节点编号+帧类型+长度低字节+长度高字节
-u8 Status_Msg[12]={0,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
+u8 Status_Msg[Reser_Len]={0,0xff,0xff,0xff};
 void UB351_Transmit(u8 *message,u16 length,u8 Frame_type)
 {	u8 Frame_check=0;
 	u16 i=0;
  if((Frame_type==3&&rx_len==0)||(Frame_type==1))
  {
-		Frame_Len=length+12+1;  
-		//if(Frame_type==3)Frame_Len=12+1;
+		Frame_Len=length+Reser_Len+1;  
 		Frame_Head[11]=(Frame_Len& 0xFF00)>>8; //长度高字节
 		Frame_Head[10]=(u8)(Frame_Len&0xFF);    //长度低字节
 		Frame_Len=0;	
@@ -32,7 +31,7 @@ void UB351_Transmit(u8 *message,u16 length,u8 Frame_type)
 		 Frame_check^=Frame_Head[i];
 			printf("%c",Frame_Head[i]);
 		}
-		for(i=0;i<12;i++)  //状态信息
+		for(i=0;i<Reser_Len;i++)  //状态信息
 		{
 		 Frame_check^=Status_Msg[i];
 			printf("%c",Status_Msg[i]);
--- USER/UB351.h
+++ USER/UB351.h
@@ -4,6 +4,7 @@
 #include "stdio.h"	
 #define Station_Number 3
 #define Station_Type 1
+#define Reser_Len 4  //±£Áô×Ö³¤¶È
 extern u8 IR_500ms;
 void UB351_Receive(u8 *message,u16 length);
 void UB351_Transmit(u8 *message,u16 length,u8 Frame_type);
